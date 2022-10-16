2b2d77e2d3c58cbc6e7661ff5bbf6f71066d2f0a
Jefferson Michael <jefferson_michael407@hotmail.com>
2022-10-16 19:02:04 -0300

    0.1 pessoa funções crud banco

---
+++ src/database/pessoa_database.py
@@ -0,0 +1,46 @@
+from src.configs.database import Database
+
+class PessoaDatabase:
+    database = Database()
+
+    @staticmethod
+    def insert(nome, cpf, telefone):
+        try:
+            PessoaDatabase.database.db_cursor.execute("INSERT INTO pessoa (nome, cpf, telefone) VALUES (?, ?, ?)", (nome, cpf, telefone))
+            PessoaDatabase.database.db_connection.commit()
+        except Exception as e:
+            print(e)
+        
+    @staticmethod
+    def delete(id):
+        try:
+            PessoaDatabase.database.db_cursor.execute("DELETE FROM pessoa WHERE id = ?", (id,))
+            PessoaDatabase.database.db_connection.commit()
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def get_all():
+        try:
+            PessoaDatabase.database.db_cursor.execute("SELECT * FROM pessoa")
+            pessoas = PessoaDatabase.database.db_cursor.fetchall()
+            return pessoas
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def get_by_id(id):
+        try:
+            PessoaDatabase.database.db_cursor.execute("SELECT * FROM pessoa WHERE id = ?", (id,))
+            pessoa = PessoaDatabase.database.db_cursor.fetchone()
+            return pessoa
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def update(id, nome, cpf, telefone):
+        try:
+            PessoaDatabase.database.db_cursor.execute("UPDATE pessoa SET nome = ?, cpf = ?, telefone = ? WHERE id = ?", (nome, cpf, telefone, id))
+            PessoaDatabase.database.db_connection.commit()
+        except Exception as e:
+            print(e)
